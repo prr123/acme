b6fd432525dcc614d5865afc8a40014a4fb2c55b
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-04-29 15:21:10 +0200

    initial commit

---
+++ certLib/certLib.go
@@ -0,0 +1,337 @@
+// library that support the generation of certificates from Lets encrypt using the DNS Challenge
+// author: prr azul software
+// date: 29 April 2023
+// copyright 2023 prr, azulsoftware
+//
+
+package certLib
+
+import (
+    "log"
+    "fmt"
+    "os"
+    "time"
+//    "net"
+    "context"
+    "crypto/ecdsa"
+    "crypto/elliptic"
+    "crypto/rand"
+    "crypto/x509"
+//    "crypto/x509/pkix"
+//    "encoding/asn1"
+    "encoding/pem"
+
+   "golang.org/x/crypto/acme"
+
+    yaml "github.com/goccy/go-yaml"
+)
+
+type CsrList struct {
+    Template string `yaml:"template"`
+    Domains []CsrDat `yaml:"domains"`
+}
+
+type CsrDat struct {
+    Domain string `yaml:"domain"`
+    Email string `yaml:"email"`
+    PemFil string `yaml:"pemfil"`
+    Name pkixName `yaml:"Name"`
+}
+
+type pkixName struct {
+    CommonName string `yaml:"CommonName"`
+    Country string `yaml:"Country"`
+    Province string `yaml:"Province"`
+    Locality string `yaml:"Locality"`
+    Organisation string `yaml:"Organisation"`
+    OrganisationUnit string `yaml:"OrganisationUnit"`
+}
+
+// yaml version of type acme.Account
+type JsAcnt struct {
+    // URI is the account unique ID, which is also a URL used to retrieve
+    // account data from the CA.
+    // When interfacing with RFC 8555-compliant CAs, URI is the "kid" field
+    // value in JWS signed requests.
+    URI string `yaml: "URI"`
+
+    // Contact is a slice of contact info used during registration.
+    // See https://tools.ietf.org/html/rfc8555#section-7.3 for supported
+    // formats.
+    Contact []string `yaml: "Contact"`
+
+    // Status indicates current account status as returned by the CA.
+    // Possible values are StatusValid, StatusDeactivated, and StatusRevoked.
+    Status string `yaml: "Status"`
+
+    // OrdersURL is a URL from which a list of orders submitted by this account
+    // can be fetched.
+    OrdersURL string `yaml: "OrdersURL"`
+
+    // The terms user has agreed to.
+    // A value not matching CurrentTerms indicates that the user hasn't agreed
+    // to the actual Terms of Service of the CA.
+    //
+    // It is non-RFC 8555 compliant. Package users can store the ToS they agree to
+    // during Client's Register call in the prompt callback function.
+    AgreedTerms string `yaml: "Terms"`
+
+    // Actual terms of a CA.
+    //
+    // It is non-RFC 8555 compliant. Use Directory's Terms field.
+    // When a CA updates their terms and requires an account agreement,
+    // a URL at which instructions to do so is available in Error's Instance field.
+    CurrentTerms string `yaml: "CurTerms"`
+
+    // Authz is the authorization URL used to initiate a new authz flow.
+    //
+    // It is non-RFC 8555 compliant. Use Directory's AuthzURL or OrderURL.
+    Authz string `yaml: "Authz"`
+
+    // Authorizations is a URI from which a list of authorizations
+    // granted to this account can be fetched via a GET request.
+    //
+    // It is non-RFC 8555 compliant and is obsoleted by OrdersURL.
+    Authorizations string `yaml: "Auth"`
+    // Certificates is a URI from which a list of certificates
+    // issued for this account can be fetched via a GET request.
+    //
+    // It is non-RFC 8555 compliant and is obsoleted by OrdersURL.
+    Certificates string `yaml: "Certs"`
+
+    // ExternalAccountBinding represents an arbitrary binding to an account of
+    // the CA which the ACME server is tied to.
+    // See https://tools.ietf.org/html/rfc8555#section-7.3.4 for more details.
+    ExternalAccountBinding *acme.ExternalAccountBinding `yaml: "ExtAcct"`
+}
+
+
+func ReadCsrFil(inFilNam string)(csrDatList *CsrList, err error) {
+
+    //todo check for yaml extension
+    bytData, err := os.ReadFile(inFilNam)
+    if err != nil {
+        return nil, fmt.Errorf("os.ReadFile: %v\n",err)
+    }
+
+    csrList := &CsrList{}
+    err = yaml.Unmarshal(bytData, csrList)
+    if err != nil {
+        return nil, fmt.Errorf("yaml Unmarshal: %v\n", err)
+    }
+
+//    PrintCsr(CsrList)
+    return csrList, nil
+}
+
+// function that creates a new client
+func NewClient(ctx context.Context, dbg bool) (cl *acme.Client, err error) {
+
+    akey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+    if err != nil { return nil, fmt.Errorf("Generate Key: %v", err)}
+
+    if dbg {log.Printf("newClient: key generated!\n")}
+
+    client := &acme.Client{Key: akey}
+    client.DirectoryURL = "https://acme-staging-v02.api.letsencrypt.org/directory"
+
+    if dbg {
+        log.Printf("Directory Url: %s\n", client.DirectoryURL)
+        log.Printf("success client created! printing client\n")
+        PrintClient(client)
+    }
+    return client, nil
+}
+
+// registers client with the acme server
+func registerClient(ctx context.Context, client *acme.Client, dbg bool)(ac *acme.Account, err error) {
+
+    acnt, err := client.Register(ctx, &acme.Account{}, acme.AcceptTOS)
+    if err != nil { return nil, fmt.Errorf("client.Register: %v", err)}
+
+    if dbg {
+        log.Printf("success CA account generated\n")
+        PrintAccount(acnt)
+    }
+
+    return acnt, nil
+}
+
+func EncodeKey(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey) (string, string) {
+    x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
+    pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: x509Encoded})
+
+    x509EncodedPub, _ := x509.MarshalPKIXPublicKey(publicKey)
+    pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
+
+    return string(pemEncoded), string(pemEncodedPub)
+}
+
+func DecodeKey(pemEncoded string, pemEncodedPub string) (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
+    block, _ := pem.Decode([]byte(pemEncoded))
+    x509Encoded := block.Bytes
+    privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
+
+    blockPub, _ := pem.Decode([]byte(pemEncodedPub))
+    x509EncodedPub := blockPub.Bytes
+    genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
+    publicKey := genericPublicKey.(*ecdsa.PublicKey)
+
+    return privateKey, publicKey
+}
+
+/*
+func testKeyEncode() {
+    privateKey, _ := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+    publicKey := &privateKey.PublicKey
+
+    encPriv, encPub := encode(privateKey, publicKey)
+
+    fmt.Println(encPriv)
+    fmt.Println(encPub)
+
+    priv2, pub2 := decode(encPriv, encPub)
+
+    if !reflect.DeepEqual(privateKey, priv2) {
+        fmt.Println("Private keys do not match.")
+    }
+    if !reflect.DeepEqual(publicKey, pub2) {
+        fmt.Println("Public keys do not match.")
+    }
+}
+*/
+
+func PrintCsr(csrlist *CsrList) {
+
+    fmt.Println("******** Csr List *********")
+    fmt.Printf("template: %s\n", csrlist.Template)
+    numDom := len(csrlist.Domains)
+    fmt.Printf("domains: %d\n", numDom)
+    for i:=0; i< numDom; i++ {
+        csrdat := csrlist.Domains[i]
+        fmt.Printf("  domain:   %s\n", csrdat.Domain)
+        fmt.Printf("  email:    %s\n", csrdat.Email)
+        fmt.Printf("  name:\n")
+        nam:= csrdat.Name
+        fmt.Printf("    CommonName:   %s\n", nam.CommonName)
+        fmt.Printf("    Country:      %s\n", nam.Country)
+        fmt.Printf("    Province:     %s\n", nam.Province)
+        fmt.Printf("    Locality:     %s\n", nam.Locality)
+        fmt.Printf("    Organisation: %s\n", nam.Organisation)
+        fmt.Printf("    OrgUnit:      %s\n", nam.OrganisationUnit)
+    }
+
+    fmt.Println("******** End Csr List *******")
+
+}
+
+
+func PrintAccount (acnt *acme.Account) {
+
+    fmt.Println("***************** Acme Account ******************")
+    fmt.Printf("URI:    %s\n", acnt.URI)
+    fmt.Printf("Status: %s\n", acnt.Status)
+    fmt.Printf("Contacts [%d]:\n", len((*acnt).Contact))
+    for i:=0; i< len((*acnt).Contact); i++ {
+        fmt.Printf("Contact[%d]: %s\n", i, (*acnt).Contact[i])
+    }
+    fmt.Printf("OrdersURL:   %s\n", acnt.OrdersURL)
+    fmt.Println (" *** non RFC 8588 terms:  ***")
+    fmt.Printf("  AgreedTerms: %s\n", acnt.AgreedTerms)
+    fmt.Printf("  Authz: %s\n", acnt.Authz)
+    fmt.Println("***************** End Account ******************")
+}
+
+func PrintJsAccount (acnt *JsAcnt) {
+
+    fmt.Println("***************** Acme JsAccount ******************")
+    fmt.Printf("URI:  %s\n", acnt.URI)
+    fmt.Printf("Contacts [%d]:\n", len((*acnt).Contact))
+    for i:=0; i< len((*acnt).Contact); i++ {
+        fmt.Printf("Contact[%d]: %s\n", i, (*acnt).Contact[i])
+    }
+    fmt.Printf("OrdersURL:   %s\n", acnt.OrdersURL)
+    fmt.Printf("AgreedTerms: %s\n", acnt.AgreedTerms)
+}
+
+func PrintClient (client *acme.Client) {
+
+    fmt.Println("************** Acme Client ******************")
+    fmt.Printf("Key: %v\n", client.Key)
+    fmt.Printf("HTTPClient: %v\n",client.HTTPClient)
+    fmt.Printf("Directory: %s\n", client.DirectoryURL)
+    fmt.Printf("Retry: %v\n", client.RetryBackoff)
+    fmt.Printf("UserAgent: %s\n",client.UserAgent)
+    fmt.Printf("KID: %s\n", client.KID)
+    fmt.Println("***************** End Client ******************")
+}
+
+func PrintAuth(auth *acme.Authorization) {
+    fmt.Println("*********** authorization ***********")
+    fmt.Printf("URI:    %s\n", auth.URI)
+    fmt.Printf("Status: %s\n", auth.Status)
+    fmt.Printf("Id typ: %s val: %s\n", auth.Identifier.Type, auth.Identifier.Value)
+    ExpTimStr:= auth.Expires.Format(time.RFC1123)
+    fmt.Printf("Expires %s\n", ExpTimStr)
+    fmt.Printf("*** Challenges[%d] ***\n", len(auth.Challenges))
+    for i, chal := range auth.Challenges {
+        fmt.Printf("   [%d]: %s URI: %s Token: %s Status: %s err: %v\n", i+1, chal.Type, chal.URI, chal.Token, chal.Status, chal.Error)
+	}
+    fmt.Println("*********** end authorization ***********")
+}
+
+func PrintDomains(domains []string) {
+    fmt.Printf("*****  domains: %d *******\n", len(domains))
+    for i, domain := range domains {
+        fmt.Printf("domain[%d]: %s\n", i+1, domain)
+    }
+    fmt.Printf("***** end domains *******\n")
+}
+
+func PrintDir(dir acme.Directory) {
+
+    fmt.Println("********** Directory **********")
+    fmt.Printf("AuthzUrl: %s\n", dir.AuthzURL)
+    fmt.Printf("OrderUrl: %s\n", dir.OrderURL)
+    fmt.Printf("RevokeUrl: %s\n", dir.RevokeURL)
+    fmt.Printf("NonceUrl: %s\n", dir.NonceURL)
+    fmt.Printf("KeyChangeUrl: %s\n", dir.KeyChangeURL)
+    fmt.Printf("Meta Terms: %v\n",  dir.Terms)
+    fmt.Printf("Meta Website: %s\n", dir.Website)
+    fmt.Printf("Meta CAA: %s\n", dir.CAA)
+    fmt.Printf("External Account Req: %v\n", dir.ExternalAccountRequired)
+    fmt.Println("******* End Directory *********")
+}
+
+func PrintOrder(ord acme.Order) {
+    fmt.Println("************ Order **************")
+    fmt.Printf("URI: %s\n", ord.URI)
+    fmt.Printf("Status: %s\n", ord.Status)
+    fmt.Printf("Expires: %s\n", ord.Expires.Format(time.RFC1123))
+    fmt.Printf("Identifiers: %d\n", len(ord.Identifiers))
+    for i:= 0; i< len(ord.Identifiers); i++ {
+        id := ord.Identifiers[i]
+        fmt.Printf("  id[%d]: typ: %s val %s\n", i+1, id.Type, id.Value)
+    }
+    fmt.Printf("Authorisation URLs: %d\n", len(ord.AuthzURLs))
+    for i:= 0; i< len(ord.AuthzURLs); i++ {
+        id := ord.AuthzURLs[i]
+        fmt.Printf("  auth for id[%d]: %s\n", i+1, id)
+    }
+    fmt.Printf("FinalizeURL: %s\n", ord.FinalizeURL)
+    fmt.Printf("CertURL: %s\n", ord.CertURL)
+    fmt.Printf("error: %v\n", ord.Error)
+    fmt.Println("********* End Order **************")
+
+}
+
+func PrintChallenge(chal *acme.Challenge, domain string) {
+    fmt.Printf("*************** %s Challenge ********\n", domain)
+    fmt.Printf("Type: %s\n", chal.Type)
+    fmt.Printf("URI:  %s\n", chal.URI)
+    fmt.Printf("Token: %s\n", chal.Token)
+    fmt.Printf("Status: %s\n", chal.Status)
+    fmt.Printf("Validated: %s\n", chal.Validated.Format(time.RFC1123))
+    fmt.Printf("Error: %v\n", chal.Error)
+    fmt.Printf("*************** End Challenge ********\n")
+}
