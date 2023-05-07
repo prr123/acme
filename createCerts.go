3bfd1846072f335e5966fae66fb27435ee90725e
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-05-07 10:38:25 +0200

    add inital test for clean-up

--- createCerts.go
+++ createCerts.go
@@ -25,10 +25,11 @@ import (
 	"os"
 	"time"
 	"net"
+	"strings"
 
 //    yaml "github.com/goccy/go-yaml"
 	"golang.org/x/crypto/acme"
-//	"github.com/cloudflare/cloudflare-go"
+	"github.com/cloudflare/cloudflare-go"
 
     cfLib "acme/acmeDns/cfLib"
 	certLib "acme/acmeDns/certLib"
@@ -38,8 +39,11 @@ import (
 func main() {
 
 	numarg := len(os.Args)
+	dbg := true
 
-	useStr := "acmeDnsTestV3 [domainfile]"
+	log.Printf("debug: %t\n", dbg)
+
+	useStr := "createCerts [domainfile]"
     zoneFilNam := "/home/peter/zones/cfDomainsShort.yaml"
 
 	csrFilNam := "csrList.yaml"
@@ -66,7 +70,7 @@ func main() {
     if err != nil {log.Fatalf("ReadZoneFileShort: %v\n", err)}
 
 	log.Printf("success reading all cf zones!\n")
-	cfLib.PrintZoneList(zoneList)
+	if dbg {cfLib.PrintZoneList(zoneList)}
 
 	numZones := len(zoneList.Zones)
 
@@ -74,16 +78,24 @@ func main() {
 	log.Printf("Acme Chal Domain Target: %d\n", numZones)
 	if numZones == 0 {log.Fatalf("no domains in file: %s\n", zoneFilNam)}
 
+
 	// read list of all domains for Acme Challenge
     csrList, err := certLib.ReadCsrFil(csrFilNam)
     if err != nil {log.Fatalf("ReadCsrFil: %v", err)}
-
-    certLib.PrintCsr(csrList)
+	log.Printf("success reading CsrFile!\n")
 
 	numAcmeDom := len(csrList.Domains)
     log.Printf("found %d acme Domains\n", numAcmeDom)
 
-	acmeDomList := make([]cfLib.ZoneShort, numAcmeDom)
+	if dbg {certLib.PrintCsr(csrList)}
+	log.Printf("certDir: %s\n", csrList.CertDir)
+
+	certDir := csrList.CertDir
+	certdir := []byte(csrList.CertDir)
+	if certdir[len(certdir) - 1] != '/' {certDir += "/"}
+	log.Printf("certDir: %s\n", certDir)
+
+	acmeDomList := make([]cfLib.ZoneAcme, numAcmeDom)
 	// see whether acme domains are in zoneList
 
 	count:=0
@@ -101,9 +113,10 @@ func main() {
 
 	if count == 0 {log.Fatalf("no matching acme domains found in cf list")}
 	numAcmeDom = count
+    log.Printf("matched %d acme Domains\n", numAcmeDom)
 
 	for j:= 0; j< numAcmeDom; j++ {
-		fmt.Printf("acme domain: %20s id: %s\n", acmeDomList[j].Name, acmeDomList[j].Id)
+		log.Printf("acme domain [%d]: %20s id: %s\n", j+1, acmeDomList[j].Name, acmeDomList[j].Id)
 	}
 
 	// get api for DNS use default yaml file
@@ -111,21 +124,83 @@ func main() {
 	if err != nil {log.Fatalf("cfLib.InitCfApi: %v\n", err)}
 	log.Printf("success: init cfapi\n")
 
+	// creating context
 	ctx := context.Background()
 
-// mod 1: replace
-//	client := acmeClient(ctx)
+	// check acme target domains for left-over acme records
+	log.Printf("checking for left-over acme records\n")
+	var listDns cloudflare.ListDNSRecordsParams
+	var rc cloudflare.ResourceContainer
+	rc.Level = cloudflare.ZoneRouteLevel
 
-	dbg := true
+	cf := cfapi.API
+
+	for i:=0; i< numAcmeDom; i++ {
+		domain := acmeDomList[i].Name
+		log.Printf("cleaning domain[%d]: %s\n", i+1, domain)
+        rc.Identifier = acmeDomList[i].Id
+
+        dnsRecs, _, err := cf.ListDNSRecords(ctx, &rc, listDns)
+        if err != nil {
+            log.Fatalf("domain[%d]: %s api.ListDNSRecords: %v\n", i+1, domain, err)
+        }
+
+		if dbg {cfLib.PrintDnsRecs(&dnsRecs)}
+        dnsId := ""
+        for j:=0; j< len(dnsRecs); j++ {
+            idx := strings.Index(dnsRecs[j].Name, "_acme-challenge.")
+            if idx == 0 {
+                dnsId = dnsRecs[j].ID
+				log.Printf("found acme challenge record[%d] in domain %s\n", j+1, domain)
+				err = cf.DeleteDNSRecord(ctx, &rc, dnsId)
+				if err != nil {log.Fatalf("api.DeleteDNSRecord: %v\n", err)}
+				log.Println("deleted Acme Dns Record")
+			}
+		}
+	}
+
+	log.Printf("finished cleaning acme records\n")
+	if dbg {
+		for i:=0; i< numAcmeDom; i++ {
+			domain := acmeDomList[i].Name
+			log.Printf("testing domain[%d]: %s\n", i+1, domain)
+        	rc.Identifier = acmeDomList[i].Id
+	        dnsRecs, _, err := cf.ListDNSRecords(ctx, &rc, listDns)
+    	    if err != nil {log.Fatalf("domain[%d]: %s api.ListDNSRecords: %v\n", i+1, domain, err)}
+			cfLib.PrintDnsRecs(&dnsRecs)
+		}
+	}
+
+	// need to test cleanup with lookup
+	for i:=0; i< numAcmeDom; i++ {
+		domain := acmeDomList[i].Name
+
+		acmeDomain := "_acme-challenge." + domain
+
+		log.Printf("waiting 2 sec before performing ns.Lookup\n")
+		time.Sleep(2 * time.Second)
+		log.Printf("performing ns.Lookup %s for DNS Challenge Record!\n", acmeDomain)
+
+		txtrecs, err := net.LookupTXT(acmeDomain)
+		if err != nil {
+			log.Printf("domain: %s -- no acme challenge record! %v", err)
+		} else {
+			log.Printf("received txtrec from Lookup\n")
+			if dbg {fmt.Printf("txtrecs [%d]: %s\n", len(txtrecs), txtrecs[0])}
+			log.Fatalf("ending!")
+		}
+	}
+
+	// create new acme client
 	client, err := certLib.NewClient(ctx, dbg)
 	if err != nil {log.Fatalf("newClient: %v\n", err)}
 
 	log.Printf("success creating acme client!\n")
-	certLib.PrintClient(client)
+	if dbg {certLib.PrintClient(client)}
 
 //	acnt, err := certLib.ReadAcmeAcnt(savActFilNam)
 //	if err != nil {
-		log.Printf("Creating Account\n")
+	log.Printf("Creating Account\n")
 		acnt, err := certLib.RegisterClient(ctx, client, dbg)
 		if err != nil {log.Fatalf("registerClient: %v\n", err)}
 		log.Printf("success registering client and creating account!")
@@ -136,14 +211,13 @@ func main() {
 //		log.Printf("Found Account File!\n")
 //	}
 
-	certLib.PrintAccount(acnt)
-
+	if dbg {certLib.PrintAccount(acnt)}
 
 	dir, err := client.Discover(ctx)
 	if err != nil {log.Fatalf("Discover error: %v\n", err)}
 
 	log.Printf("success getting client dir\n")
-	certLib.PrintDir(dir)
+	if dbg {certLib.PrintDir(dir)}
 
 
 //	numAcmeDom = 1
@@ -163,14 +237,19 @@ func main() {
 	order, err := client.AuthorizeOrder(ctx, authIdList)
 	if err != nil {log.Fatalf("client.AuthorizeOrder: %v\n",err)}
 
-	log.Printf("success getting acme orders!\n")
-	certLib.PrintOrder(*order)
+	log.Printf("received Authorization Order!\n")
+	if dbg {certLib.PrintOrder(*order)}
 
+	log.Printf("**** Begin Loop ****\n")
 	// need to loop through domains
-	for i:=0; i< numAcmeDom; i++ {
 
+//	var ctx context.Context
+
+	for i:=0; i< numAcmeDom; i++ {
 		// acmeZone.Name and domain are the same..
 		domain := authIdList[i].Value
+		log.Printf("domain [%d]: %s\n", i+1, domain)
+
 		url := order.AuthzURLs[i]
 		acmeZone := acmeDomList[i]
 
@@ -178,7 +257,7 @@ func main() {
 		if err != nil {log.Fatalf("client.GetAuthorisation: %v\n",err)}
 
 		log.Printf("success getting authorization for domain: %s\n", domain)
-		certLib.PrintAuth(auth)
+		if dbg {certLib.PrintAuth(auth)}
 
 		// Pick the DNS challenge, if any.
 		var chal *acme.Challenge
@@ -192,7 +271,7 @@ func main() {
 		if chal == nil {log.Fatalf("no dns-01 challenge avaliable for zone %s", domain)}
 
 		log.Printf("success obtaining challenge\n")
-		certLib.PrintChallenge(chal, acmeZone.Name)
+		if dbg {certLib.PrintChallenge(chal, acmeZone.Name)}
 
 		// Fulfill the challenge.
 		val, err := client.DNS01ChallengeRecord(chal.Token)
@@ -202,22 +281,30 @@ func main() {
 
 		recId, err := cfapi.AddDnsChalRecord(acmeZone, val)
 		if err != nil {log.Fatalf("AddDnsChalRecord: %v", err)}
+		acmeDomList[i].AcmeId = recId
+
+		if dbg {
+			rc.Identifier = acmeDomList[i].Id
+	        dnsRecs, _, err := cf.ListDNSRecords(ctx, &rc, listDns)
+    	    if err != nil {log.Fatalf("domain[%d]: %s api.ListDNSRecords: %v\n", i+1, domain, err)}
+			cfLib.PrintDnsRecs(&dnsRecs)
+		}
 
 		log.Printf("success creating dns record!")
 
 		// check DNS Record via LookUp
 		acmeDomain := "_acme-challenge." + domain
 
-		log.Printf("doing ns.Lookup %s for Challenge Record!\n", acmeDomain)
-		log.Printf("wait 2 sec\n")
+		log.Printf("waiting 2 sec before performing ns.Lookup\n")
 		time.Sleep(2 * time.Second)
-		log.Printf("continue: LookUp acme Dns Rec\n")
+		log.Printf("performing ns.Lookup %s for DNS Challenge Record!\n", acmeDomain)
 
 		rdAttempt := -1
 		for i:= 0; i< 5; i++ {
 			txtrecs, err := net.LookupTXT(acmeDomain)
 			if err == nil {
-				fmt.Printf("txtrecs [%d]: %s\n", len(txtrecs), txtrecs[0])
+				log.Printf("received txtrec from Lookup\n")
+				if dbg {fmt.Printf("txtrecs [%d]: %s\n", len(txtrecs), txtrecs[0])}
 				rdAttempt = i;
 				break
 			} else {
@@ -227,33 +314,52 @@ func main() {
 		}
 
 		if rdAttempt < 0 {log.Fatalf("could not find acme record: %v", err)}
-		log.Printf("Lookup successful in %d attempts!\n", rdAttempt)
+		log.Printf("Lookup successful in %d attempts!\n", rdAttempt +1)
 
 		// Let CA know we're ready. But are we? Is DNS propagated yet?
 		log.Printf("sending Accept\n")
-		if _, err := client.Accept(ctx, chal); err != nil {
+		chal2, err := client.Accept(ctx, chal) 
+		if err != nil {
 			log.Fatalf("dns-01 accept for %q: %v", domain, err)
 		}
+		if dbg {certLib.PrintChallenge(chal2, domain)}
+    	log.Printf("Accept acknowledged\n")
+		orderUrl := order.URI
+		tmpord, err := client.GetOrder(ctx, orderUrl)
+		if err !=nil {
+			log.Fatalf("order error: %v\n", err)
+		}
+		if dbg {certLib.PrintOrder(*tmpord)}
+	}
 
-		url = order.URI
-    	log.Printf("**** waiting for order for domain: %s ****\n", url)
-    	ord2, err := client.WaitOrder(ctx, url)
-    	if err != nil {log.Fatalf("client.WaitOrder: %v\n",err)}
+	os.Exit(1)
 
-    	certLib.PrintOrder(*ord2)
-		err = cfapi.DelDnsChalRecord(acmeZone, recId)
-    	if err != nil {log.Fatalf("DelDnsChalRecord: %v\n",err)}
-		log.Printf("deleted DNS Chal Record for zone: %s\n", acmeZone.Name)
+    log.Printf("waiting for order\n")
+	certUrl := order.URI
+	if dbg {log.Printf("order url: %s\n", certUrl)}
+
+    	ord2, err := client.WaitOrder(ctx, certUrl)
+    	if err != nil {
+			if ord2 != nil {certLib.PrintOrder(*ord2)}
+			log.Fatalf("client.WaitOrder: %v\n",err)
+		}
+		log.Printf("received order!\n")
+
+//		if dbg {certLib.PrintOrder(*ord2)}
+
+	for i:=0; i< numAcmeDom; i++ {
 
 		csrData := csrList.Domains[i]
-//    	certLib.PrintCsr(csrData)
-
+		//certLib.PrintCsr(csrData)
+		domain := csrData.Domain
+		log.Printf("generating certificate for domain: %s\n", domain)
 		// get certificates
 		certNam, err :=certLib.GenerateCertName(domain)
 		if err != nil {log.Fatalf("GenerateCertName: %v", err)}
 
-		keyFilNam := certNam + ".key"
-		certFilNam := certNam + ".crt"
+		keyFilNam := certDir + certNam + ".key"
+		certFilNam := certDir + certNam + ".crt"
+		log.Printf("key file: %s cert file: %s\n", keyFilNam, certFilNam)
 
 		certKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 		if err != nil {
@@ -275,7 +381,6 @@ func main() {
 		// need to compare csrParse and template
 		fmt.Printf("csrParse: %v\n", csrParse)
 
-
 		ordUrl := ord2.FinalizeURL
 
 		derCerts, certUrl, err := client.CreateOrderCert(ctx, ordUrl, csr, true)
@@ -288,6 +393,15 @@ func main() {
 
 		err = certLib.SaveCertsPem(derCerts, certFilNam)
         if err != nil {log.Fatalf("SaveCerts: %v\n",err)}
+
+		// cleanup
+
+		acmeZone := acmeDomList[i]
+
+		err = cfapi.DelDnsChalRecord(acmeZone)
+    	if err != nil {log.Fatalf("DelDnsChalRecord: %v\n",err)}
+		log.Printf("deleted DNS Chal Record for zone: %s\n", acmeZone.Name)
+
 	}
 
 	log.Printf("success\n")
