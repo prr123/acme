1ed5dc921fa4abc6846ad333ee8f0579541f4446
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-06-11 15:09:19 +0200

    initial commit

--- creLEAcnt.go
+++ creLEAcnt.go
@@ -1,4 +1,4 @@
-// creLEAcnt.go
+// creAcnt.go
 // program that generates Lets encrypt Account and saves keys
 // author: prr azul software
 // date: 8 May 2023
@@ -12,6 +12,7 @@ import (
 	"log"
 	"fmt"
 	"os"
+//	"time"
 
 	certLib "acme/acmeDns/certLib"
 )
@@ -25,7 +26,7 @@ func main() {
 	log.Printf("debug: %t\n", dbg)
 
 	useStr := "creLEAcnt"
-
+	helpStr := "help:\nprogram that creates an account with Let's Encrypt\n"
 
 	if numarg > 2 {
 		fmt.Println(useStr)
@@ -35,24 +36,25 @@ func main() {
 
 	if numarg == 2 {
 		if os.Args[1] == "help" {
+			fmt.Println(helpStr)
 			fmt.Println(useStr)
 			os.Exit(1)
 		}
+		log.Printf("invalid argument!\n")
+		os.Exit(1)
 	}
 
+	leAcnt, err := certLib.CreateLEAccount()
+	if err != nil {log.Fatalf("CreateLEAccount: %v\n", err)}
 
-	leobj, err := certLib.CreateNewLEAccount()
-	if err != nil {log.Fatalf("CreateNewAccount: %v\n", err)}
 
-	client := leobj.Client
-	if leobj.Dbg {certLib.PrintAccount(leobj.Acnt)}
+	if leAcnt.Dbg {certLib.PrintLEAcnt(leAcnt)}
 
-	ctx := context.Background()
-	ledir, err := client.Discover(ctx)
+	client := leAcnt.Client
+	dir, err := client.Discover(context.Background())
 	if err != nil {log.Fatalf("Discover error: %v\n", err)}
 
 	log.Printf("success getting client dir\n")
-	if leobj.Dbg {certLib.PrintDir(ledir)}
+	if dbg {certLib.PrintDir(dir)}
 
 }
-
