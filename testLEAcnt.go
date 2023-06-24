aac1c5c2e1c5aef696cb11f8b84c6d31515876af
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-06-24 12:57:35 +0200

    errors

--- testLEAcnt.go
+++ testLEAcnt.go
@@ -15,6 +15,7 @@ import (
 	"os"
 
 	certLib "acme/acmeDns/certLib"
+    util "github.com/prr123/utility/utilLib"
 )
 
 
@@ -24,29 +25,46 @@ func main() {
 	dbg := true
 
 	log.Printf("debug: %t\n", dbg)
+    flags:=[]string{"dbg", "acnt"}
+    acntFil := "LEAcnt.yaml"
 
-	useStr := "testAcnt"
+    useStr := "testLEAcnt [/acnt=file] [/dbg]"
+    helpStr := "help:\nprogram that reads a account file with Let's Encrypt CA\n"
 
-//	zoneFilNam := "/home/peter/zones/cfDomainsShort.yaml"
-//	csrFilNam := "csrList.yaml"
-
-	if numarg > 2 {
-		fmt.Println(useStr)
+	if numarg > 3 {
+		fmt.Printf("usage: %s\n",useStr)
 		fmt.Println("too many arguments in cl!")
 		os.Exit(-1)
 	}
 
-	if numarg == 2 {
+	if numarg > 1{
 		if os.Args[1] == "help" {
-			fmt.Println(useStr)
-			os.Exit(1)
+            fmt.Println(helpStr)
+            fmt.Printf("usage: %s\n",useStr)
+            os.Exit(1)
 		}
+		flagMap, err := util.ParseFlags(os.Args, flags)
+		if err != nil {log.Fatalf("util.ParseFlags: %v\n", err)}
+
+		_, ok := flagMap["dbg"]
+		if ok {dbg = true}
+		if dbg {
+			for k, v :=range flagMap {
+				fmt.Printf("flag: %s value: %s\n", k, v)
+			}
+		}
+        val, ok := flagMap["acnt"]
+        if ok {
+            acntFil = val.(string)
+        }
+
+
 	}
 
 	// creating context
 	ctx := context.Background()
 
-	client, err := certLib.GetLEClient()
+	client, err := certLib.GetLEClient(acntFil, true)
 	if err != nil {log.Fatalf("getLEClient: %v\n", err)}
 
 	certLib.PrintClient(client)
