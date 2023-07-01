82c0e2a7dd1933d53e2a8e8093ec72de9eb3f4f2
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-07-01 17:19:21 +0200

    update printCert

--- certLib/certLib.go
+++ certLib/certLib.go
@@ -539,21 +539,71 @@ func SaveCertsPem(derCerts [][]byte, certFile string)(err error){
 			Bytes: certs[i].Raw,
 		}))))
 
-        }
-/*
-	for _, c := range certs {
-		pemData = append(pemData, strings.TrimSpace(string(pem.EncodeToMemory(&pem.Block{
-			Type:  "CERTIFICATE",
-			Bytes: c.Raw,
-		}))))
 	}
-*/
+
 	if err := os.WriteFile(certFile, []byte(strings.Join(pemData, "\n")), 0600); err != nil {
 		return fmt.Errorf("Error writing certificate file %q: %v", certFile, err)
 	}
 
 	return nil
 }
+
+func ReadPemCerts(certFile string, dbg bool)(err error){
+
+	pemData, err := os.ReadFile(certFile)
+	if err != nil {return fmt.Errorf("os.ReadFile: %v\n", err)}
+	if dbg {fmt.Println("********** start pem file ********")}
+
+	restStart := pemData
+
+	certCount:=0
+	for i:=0; i< 10; i++ {
+		block, rest := pem.Decode(restStart)
+		if block == nil {
+			certCount = i
+			break
+		}
+		if dbg {
+			fmt.Printf("*** Block[%d] Type: %s ***\n", i+1, block.Type)
+			fmt.Printf("  Headers[%d]\n", len(block.Headers))
+		}
+		for k,v := range block.Headers {
+			if dbg {fmt.Printf("  header: %s vlaue %s\n", k, v)}
+		}
+
+		derByt := block.Bytes
+		cert, err := x509.ParseCertificate(derByt)
+		if err != nil {return fmt.Errorf("x509.ParseCertificates: %v", err)}
+		PrintCertInfo(*cert, i)
+
+		if dbg {fmt.Printf("*******  End Block ******\n")}
+
+		restStart = rest
+	}
+
+	if dbg {
+		fmt.Printf("  Certificates: %d\n", certCount)
+		fmt.Println("********** end pem file ********")
+	}
+	return nil
+}
+
+
+func ParseCertsInfo(derCerts [][]byte, certInfoFilnam string)(err error){
+
+	certs := make([]*x509.Certificate, len(derCerts))
+	log.Printf("certs: %d\n", len(derCerts))
+
+	for i, asn1Data := range derCerts {
+		certs[i], err = x509.ParseCertificate(asn1Data)
+		if err != nil {return fmt.Errorf("Cert [%d]: %v",i, err)}
+		PrintCertInfo(*certs[i], i)
+
+	}
+
+	return nil
+}
+
 
 // create certficate sign request
 func CreateCsrTpl(csrData CsrDat) (template x509.CertificateRequest) {
@@ -983,22 +1033,137 @@ func PrintOrder(ord acme.Order) {
 
 }
 
+func PrintCertInfo(cert x509.Certificate, i int){
+
+/*
+	sigAlgo := []string {
+		"Unknown",
+		"MD2WithRSA",  // Unsupported.
+		"MD5WithRSA",  // Only supported for signing, not verification.
+		"SHA1WithRSA", // Only supported for signing, and verification of CRLs, CSRs, and OCSP responses.
+		"SHA256WithRSA",
+		"SHA384WithRSA",
+		"SHA512WithRSA",
+		"DSAWithSHA1",   // Unsupported.
+		"DSAWithSHA256", // Unsupported.
+		"ECDSAWithSHA1", // Only supported for signing, and verification of CRLs, CSRs, and OCSP responses.
+		"ECDSAWithSHA256",
+		"ECDSAWithSHA384",
+		"ECDSAWithSHA512",
+		"SHA256WithRSAPSS",
+		"SHA384WithRSAPSS",
+		"SHA512WithRSAPSS",
+		"PureEd25519",
+	}
+
+	pubKeyAlgo := []string{
+		"Unknown",
+		"RSA",
+		"DSA", // Only supported for parsing.
+		"ECDSA",
+		"Ed25519",
+	}
+*/
+	fmt.Printf("******** Cert %d ********\n", i+1)
+	fmt.Printf("version: %d\n", cert.Version)
+	fmt.Printf("SerialNum: %s\n", (*cert.SerialNumber).String())
+
+	fmt.Printf("Signature Algo:    %s\n", cert.SignatureAlgorithm.String())
+	fmt.Printf("Public Key Algo: %s\n", cert.PublicKeyAlgorithm.String())
+//	fmt.Printf("Signature Algo[%d]: %s\n", cert.SignatureAlgorithm, sigAlgo[cert.SignatureAlgorithm])
+//	fmt.Printf("Public Key Algo[%d]: %s\n", cert.PublicKeyAlgorithm, pubKeyAlgo[cert.PublicKeyAlgorithm])
+
+	fmt.Printf("Not Before: %s\n", cert.NotBefore.Format(time.RFC1123))
+	fmt.Printf("Not After:  %s\n", cert.NotAfter.Format(time.RFC1123))
+
+
+	fmt.Printf("Issuer IsCA: %t\n", cert.IsCA)
+//	fmt.Printf("\nIssuer: \n")
+	PrintPkixNam(cert.Issuer)
+
+	fmt.Printf("\nSubject: \n")
+	PrintPkixNam(cert.Subject)
+
+	fmt.Printf("DNS Names  [%d]:\n", len(cert.DNSNames))
+	if len(cert.DNSNames) > 0 {
+		for k:=0; k< len(cert.DNSNames); k++ {
+			fmt.Printf("  %d: %s\n", k+1, cert.DNSNames[k])
+		}
+	}
+	fmt.Printf("Email Adrs [%d]:\n", len(cert.EmailAddresses))
+	fmt.Printf("IP Adrs [%d]:\n", len(cert.IPAddresses))
+	if len(cert.IPAddresses) > 0 {
+		for k:=0; k< len(cert.IPAddresses); k++ {
+			fmt.Printf("  %d: %s\n", k+1, cert.IPAddresses[k])
+		}
+	}
+	fmt.Printf("URIs [%d]:\n", len(cert.URIs))
+	if len(cert.URIs) > 0 {
+		for k:=0; k< len(cert.URIs); k++ {
+			fmt.Printf("  %d: %s\n", k+1, *cert.URIs[k])
+		}
+	}
+
+	fmt.Printf("Name Constraints:\n")
+	fmt.Printf("  PermittedDNSDomains [%d]:\n", len(cert.PermittedDNSDomains))
+	if len(cert.PermittedDNSDomains) > 0 {
+		for k:=0; k< len(cert.PermittedDNSDomains); k++ {
+			fmt.Printf("    %d: %s\n", k+1, cert.PermittedDNSDomains[k])
+		}
+	}
+	fmt.Printf("  ExcludedDNSDomains [%d]:\n", len(cert.ExcludedDNSDomains))
+	if len(cert.ExcludedDNSDomains) > 0 {
+		for k:=0; k< len(cert.ExcludedDNSDomains); k++ {
+			fmt.Printf("    %d: %s\n", k+1, cert.ExcludedDNSDomains[k])
+		}
+	}
+
+//	fmt.Printf("Extensions[%d]:\n", len(cert.Extensions))
+	switch len(cert.Extensions) {
+	case 0:
+		fmt.Printf("Extensions: ---\n")
+	case 1:
+		ext := cert.Extensions[0]
+		fmt.Printf("Extension: Id %d Value: %v Critical: %t\n", ext.Id, ext.Value, ext.Critical)
+	default:
+		fmt.Printf("Extensions: %d\n", len(cert.Extensions))
+		for k:=0; k< len(cert.Extensions); k++ {
+			ext := cert.Extensions[k]
+			fmt.Printf("  %d: Id %d Value: %v Critical: %t\n", k+1, ext.Id, ext.Value, ext.Critical)
+		}
+	}
+	fmt.Printf("Extra Extensions[%d]:\n", len(cert.ExtraExtensions))
+	fmt.Printf("Unhandled Crtical Extensions[%d]:\n", len(cert.UnhandledCriticalExtensions))
+
+	if len(cert.OCSPServer) == 0 {
+		fmt.Printf("OCSPServer: ---\n")
+	} else {
+		fmt.Printf("OCSPServer [%d]: \n",len(cert.OCSPServer))
+		for i:=0; i<len(cert.OCSPServer); i++ {
+			fmt.Printf("  %d: %s\n", i+1, cert.OCSPServer[i])
+		}
+	}
+	if len(cert.IssuingCertificateURL) == 0 {
+		fmt.Printf("Issuing Certificate URLs: ---\n")
+	} else {
+		fmt.Printf("Issuing Certificate URLs [%d]: \n", len(cert.IssuingCertificateURL))
+		for i:=0; i< len(cert.IssuingCertificateURL); i++ {
+			fmt.Printf("  %d: %s\n", i+1, cert.IssuingCertificateURL[i])
+		}
+	}
+	fmt.Printf("******* End Cert ********\n")
+}
 
 func PrintCert(cert *x509.Certificate) {
 
 	fmt.Println("************ Certificate **************")
 
 	fmt.Printf("Version: %d\n", cert.Version)
-	fmt.Printf("Serial:  %d\n", cert.SerialNumber)
+	fmt.Printf("Serial:  %s\n", (*cert.SerialNumber).String())
 	fmt.Printf("Sig Algo:    %s\n", cert.SignatureAlgorithm.String())
 	fmt.Printf("PubKey Algo: %s\n", cert.PublicKeyAlgorithm.String())
-	fmt.Printf("Issuer: \n")
-	namIssuer := cert.Issuer
-	fmt.Printf("  Countries: %d\n", len(namIssuer.Country))
-	if len(namIssuer.Country)>0 {
-		fmt.Printf("    Country: %s\n", namIssuer.Country[0])
-	}
-	fmt.Printf("Issuer: \n")
+
+	fmt.Printf("Issuer: %t\n", cert.IsCA)
 	PrintPkixNam(cert.Issuer)
 
 	fmt.Printf("Subject: \n")
@@ -1016,76 +1181,130 @@ func PrintCert(cert *x509.Certificate) {
 		fmt.Printf("    %d:%s\n", i+1, cert.IPAddresses[i])
 	}
 
+	fmt.Printf("Extensions[%d]:\n", len(cert.Extensions))
+	fmt.Printf("Extra Extensions[%d]:\n", len(cert.ExtraExtensions))
+	fmt.Printf("Unhandled Crtical Extensions[%d]:\n", len(cert.UnhandledCriticalExtensions))
+
+	fmt.Printf("OCSPServer [%d]: \n", len(cert.OCSPServer))
+	fmt.Printf("Issuing Certificate URLs [%d]: \n", len(cert.IssuingCertificateURL))
+
 	fmt.Println("********** End Certificate ************")
 
 }
 
 func PrintPkixNam(subj pkix.Name) {
-    fmt.Printf("  Serial Number: %s\n", subj.SerialNumber)
-    fmt.Printf("  CommonName: %s\n", subj.CommonName)
-
-	if len(subj.Country) ==1 {
-    	fmt.Printf("  Country %s\n", subj.Country[0])
+	if len(subj.SerialNumber) == 0 {
+		fmt.Printf("  Serial Number: ---\n")
 	} else {
-	    fmt.Printf("  Country %d\n", len(subj.Country))
+		fmt.Printf("  Serial Number: %s\n", subj.SerialNumber)
+	}
+
+	if len(subj.CommonName) ==0 {
+		fmt.Printf("  CommonName: ---\n")
+	} else {
+		fmt.Printf("  CommonName: %s\n", subj.CommonName)
+	}
+
+	switch len(subj.Country) {
+	case 0:
+    	fmt.Printf("  Country: --\n")
+	case 1: 
+    	fmt.Printf("  Country: %s\n", subj.Country[0])
+	default:
+	    fmt.Printf("  Countries[%d]:\n", len(subj.Country))
 	    for i:=0; i< len(subj.Country); i++ {
     	    fmt.Printf("%d: %s\n", i+1, subj.Country[i])
     	}
 	}
-	if len(subj.Organization) ==1 {
+
+	switch len(subj.Organization) {
+	case 0:
+		fmt.Printf("  Organization: ---\n")
+	case 1:
 		fmt.Printf("  Organization: %s\n",subj.Organization[0])
-	} else {
-		fmt.Printf("  Organization %d\n", len(subj.Organization))
+	default:
+		fmt.Printf("  Organizations[%d]:\n", len(subj.Organization))
     	for i:=0; i< len(subj.Organization); i++ {
         	fmt.Printf("%d: %s\n", i+1, subj.Organization[i])
     	}
 	}
 
-	if len(subj.Locality) == 1 {
-	    fmt.Printf("  Locality %s\n", subj.Locality[0])
-	} else {
-    	fmt.Printf("  Locality %d\n", len(subj.Locality))
+	switch len(subj.OrganizationalUnit) {
+	case 0:
+		fmt.Printf("  Organizational Unit: ---\n")
+	case 1:
+		fmt.Printf("  Organizational Unit: %s\n",subj.OrganizationalUnit[0])
+	default:
+		fmt.Printf("  Organizational Units[%d]:\n", len(subj.OrganizationalUnit))
+    	for i:=0; i< len(subj.OrganizationalUnit); i++ {
+        	fmt.Printf("%d: %s\n", i+1, subj.OrganizationalUnit[i])
+    	}
+	}
+
+	switch len(subj.Locality) {
+	case 0:
+	    fmt.Printf("  Locality: ---\n")
+	case 1:
+	    fmt.Printf("  Locality: %s\n", subj.Locality[0])
+	default:
+    	fmt.Printf("  Localities[%d]:\n", len(subj.Locality))
     	for i:=0; i< len(subj.Locality); i++ {
         	fmt.Printf("%d: %s\n", i+1, subj.Locality[i])
     	}
 	}
 
-	if len(subj.Province) == 1 {
-	    fmt.Printf("  Province %s\n", subj.Province[0])
-	} else {
-    	fmt.Printf("  Province %d\n", len(subj.Province))
+	switch len(subj.Province) {
+	case 0:
+	    fmt.Printf("  Province: ---\n")
+	case 1:
+	    fmt.Printf("  Province: %s\n", subj.Province[0])
+	default:
+    	fmt.Printf("  Provinces[%d]:\n", len(subj.Province))
     	for i:=0; i< len(subj.Province); i++ {
         	fmt.Printf("%d: %s\n", i+1, subj.Province[i])
     	}
 	}
 
-	if len(subj.StreetAddress) == 1 {
-	    fmt.Printf("  StreetAddress %s\n", subj.StreetAddress[0])
-	} else {
-    	fmt.Printf("  StreetAddress %d\n", len(subj.StreetAddress))
+	switch len(subj.StreetAddress) {
+	case 0:
+	    fmt.Printf("  StreetAddress: ---\n")
+	case 1:
+	    fmt.Printf("  StreetAddress: %s\n", subj.StreetAddress[0])
+	default:
+    	fmt.Printf("  StreetAddresses[%d]:\n", len(subj.StreetAddress))
     	for i:=0; i< len(subj.StreetAddress); i++ {
         	fmt.Printf("%d: %s\n", i+1, subj.StreetAddress[i])
     	}
 	}
 
-	if len(subj.PostalCode) == 1 {
-	    fmt.Printf("  PostalCode %s\n", subj.PostalCode[0])
-	} else {
-    	fmt.Printf("  PostalCode %d\n", len(subj.PostalCode))
+	switch len(subj.PostalCode) {
+	case 0:
+	    fmt.Printf("  PostalCode: ---\n")
+	case 1:
+	    fmt.Printf("  PostalCode: %s\n", subj.PostalCode[0])
+	default:
+    	fmt.Printf("  PostalCodes[%d]\n", len(subj.PostalCode))
     	for i:=0; i< len(subj.PostalCode); i++ {
         	fmt.Printf("%d: %s\n", i+1, subj.PostalCode[i])
     	}
 	}
 
+	if len(subj.Names) == 0 {
+	    fmt.Printf("  Names: ---\n")
+	} else {
+	    fmt.Printf("  Names[%d]:\n", len(subj.Names))
+    	for i:=0; i< len(subj.Names); i++ {
+        	fmt.Printf("%d: %v\n", i+1, subj.Names[i])
+    	}
+	}
 
-    fmt.Printf("  Names %d\n", len(subj.Names))
-    for i:=0; i< len(subj.Names); i++ {
-        fmt.Printf("%d: %v\n", i+1, subj.Names[i])
-    }
-    fmt.Printf("  ExtraNames %d\n", len(subj.ExtraNames))
-    for i:=0; i< len(subj.ExtraNames); i++ {
-        fmt.Printf("Subject:\n")
-        fmt.Printf("%d: %v\n", i+1, subj.ExtraNames[i])
+	if len(subj.ExtraNames) == 0 {
+		fmt.Printf("  ExtraNames: ---\n")
+	} else {
+	    fmt.Printf("  ExtraNames{%d]:\n", len(subj.ExtraNames))
+    	for i:=0; i< len(subj.ExtraNames); i++ {
+        	fmt.Printf("%d: %v\n", i+1, subj.ExtraNames[i])
+		}
     }
 }
 
